c8770ff0498fe6173bdc4d63cbae305c32a445db
Ed Webber <edwebber06@gmail.com>
2019-05-21 21:22:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 # Add  code here!
 def prime?(number)
-  if num<0|| num == 0 || num == 1
+  if number<0|| number == 0 || number == 1
     return false
   else 
     
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+    (2..number-1).to_a.all? do |possible_factor|
+      number % possible_factor != 0
     end
   end
 end
