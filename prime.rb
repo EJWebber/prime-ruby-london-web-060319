9dac0a65bf690902ef4a2f6d71b017e9b1b83541
Ed Webber <edwebber06@gmail.com>
2019-05-21 22:03:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,10 @@ if number <0||number==0||number==1
 elsif
 number==2||number==3||number==5||number==7
 return true
+elsif
+number%2=true||number%3=true||number%4=true||number%5=true||number%6=true||number%7=true
+return false
 else
-
+  return true
 end
 end
